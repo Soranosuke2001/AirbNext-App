73c3e9ca7b61cb4894842c928b3cd2dc3888dc41
sora <sorazora1.4@gmail.com>
2023-04-21 20:36:27 -0700

    completed the navbar

--- app/components/Navbar/MenuItem.tsx
+++ app/components/Navbar/MenuItem.tsx
@@ -1,17 +1,22 @@
-"use client"
+"use client";
 
-import { La_Belle_Aurore } from 'next/font/google';
-import { FC } from 'react'
+import { La_Belle_Aurore } from "next/font/google";
+import { FC } from "react";
 
 interface MenuItemProps {
   onClick: () => void;
   label: string;
 }
 
-const MenuItem: FC<MenuItemProps> = ({ onClick, label}) => {
-  return <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
-    {label}
-  </div>
-}
+const MenuItem: FC<MenuItemProps> = ({ onClick, label }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+    >
+      {label}
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
