f9547702caeeea4ae7f4786e38983d25123cf4b6
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-23 20:55:31 -0700

    added more details to each listing on the home page

--- app/components/Listings/ListingCard.tsx
+++ app/components/Listings/ListingCard.tsx
@@ -8,6 +8,7 @@ import { FC, useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -78,10 +79,28 @@ const ListingCard: FC<ListingCardProps> = ({
             src={data.imageSrc}
             className="object-cover h-full w-full group-hover:scale-110 transition"
           />
-          <div className='absolute top-3 right-3'>
+          <div className="absolute top-3 right-3">
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">/ night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
