ca82071e1351bac023d6c38afe904b6f0ff2e400
Soranosuke2001 <sorazora1.4@gmail.com>
2023-04-23 22:06:38 -0700

    now renders the listing image and like button is functional in the individual listing page

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,11 +1,40 @@
-import { FC } from 'react'
+"use client"
+
+import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/Listings/ListingHead";
+import { categories } from "@/app/libs/categoryList";
+import { SafeListing, SafeUser } from "@/app/types";
+import { Reservation } from "@prisma/client";
+import { FC, useMemo } from "react";
 
 interface ListingClientProps {
-  
+  reservation?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser;
+  };
+  currentUser: SafeUser | null;
 }
 
-const ListingClient: FC<ListingClientProps> = ({}) => {
-  return <div>ListingClient</div>
-}
+const ListingClient: FC<ListingClientProps> = ({ listing, currentUser }) => {
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category);
+  }, [listing.category]);
 
-export default ListingClient
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-8">
+          <ListingHead
+            id={listing.id}
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default ListingClient;
